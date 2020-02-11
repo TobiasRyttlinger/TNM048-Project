403ec1854b715d48e21dffc56178c4007bd25d08
Tim Cardell <timcal04.04@gmail.com>
2020-02-11 13:42:02 +0100

    .

--- hello.py
+++ hello.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    message = "Hello, World"
-    return render_template('index.html', message=message)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def hello_world():
+    return "hej"
