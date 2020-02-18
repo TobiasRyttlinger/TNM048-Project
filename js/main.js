55305aa92751ee692f9664bb91707fb0dd06b3e0
gunnarsdotter <gunnarsdotter@live.com>
2020-02-18 22:25:58 +0100

    throw all, start new, the map is working

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var world_map, focus_plus_context, points,dbscanner
 
 d3.csv("data/NYPD_Complaint_Data_Historic.csv", function(data){
     data = parseData(data);
-
+    console.log(data)
     dbscanner = DBSCAN().data(data.features).eps(0.2).minPts(5);
     var point_assignment_result = dbscanner();
     console.log('Resulting DBSCAN output', point_assignment_result);
@@ -21,9 +21,18 @@ d3.csv("data/NYPD_Complaint_Data_Historic.csv", function(data){
 
 function parseData(data){
     var d = [];
+    var i = 0;
     var timeParse = d3.timeParse("%d/%m/%Y");
     data.forEach(element => {
-        d.push(
+        
+       
+        d.push({geometry:{
+            type:"Point",
+            coordinates:[
+             parseFloat(element.Latitude),
+             parseFloat(element.Longitude)
+            ]
+        },type:"Feature", id: i++, properties:
             {
                 Latitude: parseFloat(element.Latitude),
                 Longitude: parseFloat(element.Longitude),
@@ -34,10 +43,11 @@ function parseData(data){
                 Type: element.OFNS_DESC,
                 KeyCord: parseInt(element.KY_CD),
                 Boro: element.BORO_NM,
-            }
+            }}
             );
+        
     });
-    var datany = {features: d, column: data.column};
+    var datany = {type: "FeatureCollection", features: d};
     return datany;
 
 }
