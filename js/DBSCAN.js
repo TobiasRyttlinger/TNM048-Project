85565eb0885c1bb3e3b56dd0f66eb32f47fc2c7e
Tobias Ryttlinger <tosryr@gmail.com>
2020-02-20 10:36:01 +0100

    removed haversine unnecesary

--- js/DBSCAN.js
+++ js/DBSCAN.js
@@ -13,33 +13,6 @@
 				return Math.sqrt(Math.pow((point1.Latitude - point2.Latitude), 2) + Math.pow((point1.Longitude - point2.Longitude), 2));
 		};
 
-		function haversine_distance(point1, point2) {
-					// default 4 sig figs reflects typical 0.3% accuracy of spherical model
-					if (typeof precision === 'undefined') {
-						var precision = 4;
-					}
-
-					var R = 6371;
-					var lat1 = point1.Latitude * Math.PI / 180,
-						lon1 = point1.Longitude * Math.PI / 180;
-					var lat2 = point2.Latitude * Math.PI / 180,
-						lon2 = point2.Longitude * Math.PI / 180;
-
-					var dLat = lat2 - lat1;
-					var dLon = lon2 - lon1;
-
-					var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-						Math.cos(lat1) * Math.cos(lat2) *
-						Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-					var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-					var d = R * c;
-
-					return d.toPrecision(precision);
-				}
-
-
-
 		function getNeighbours(point_index){
 				neighbours = [];
 				var d = data[point_index];
@@ -48,6 +21,7 @@
 					if(point_index === i){
 						continue;
 					}
+
 					if(distance(data[i].properties,d.properties) <= eps){
 						neighbours.push(i);
 					}
